14557eb5c54d8dfff585782085279565919231c7
Suhrid Mulay <suhridmulay@gmail.com>
2020-09-23 12:03:42 +0530

    Added facility to execute simple command sequentially, now usable as an elementary shell

--- src/terminal.c
+++ src/terminal.c
@@ -6,9 +6,26 @@
 #define EXIT_CODE "quit"
 #define PATH_SIZE 256
 
+#define ARGC_MAX 15
+
 #define PROMPT ">>>"
 
 char curr_path[PATH_SIZE];
+
+struct statement {
+    // A single commands
+    char ** command;
+    // Or two statements joined with an operator
+    // Operator codes: 0 for serial
+    //                 1 for parallel
+    int operator;
+    struct statement * operands[2];
+};
+
+void execute_stmt(struct statement);
+
+void execute_wait(char ** command);
+void execute_parallel(char ** command);
 
 int main() {
     // Initialisation
@@ -41,7 +58,63 @@ int main() {
             running = 0;
         } else {
             // Run the input from the linebuffer
-            printf("You wanted to run: %s\n", linebuf);
+            printf("Tokenising input...\n");
+            // Break it into tokens
+            char * token = strtok(linebuf, " ");
+            char ** cmd = malloc(sizeof(char *) * ARGC_MAX);
+            printf("Executable: %s\n", token);
+            int i = 0;
+            cmd[i] = token;
+            i++;
+            while(token != NULL) {
+                token = strtok(NULL, " ");
+                printf("Argument: %s\n", token);
+                cmd[i] = token;
+                i++;
+            }
+            printf("Attempting execution\n");
+            execute_wait(cmd);
+        }
+    }
+}
+
+void execute_wait(char ** command) {
+    int pid = fork();
+    if (pid == 0) {
+        // Inside child
+        // -- Start running said command
+        int res = execvp(command[0], &command[0]);
+        if (res == -1) {
+            printf("Error loading executable: %s\n", command[0]);
+        }
+        printf("Process loading result: %d\n", res);
+    } else if (pid > 0) {
+        // Inside parent
+        // -- Wait for the child
+        int status;
+        wait(&status);
+        printf("Wait status: %d\n", status);
+    }
+}
+
+void execute_parallel(char ** command) {
+    int pid = fork();
+    if (pid == 0) {
+        // Inside child
+        // -- Start running said command
+        int res = execvp(command[0], &command[0]);
+    } else if (pid > 0) {
+        // Inside parent
+        // -- Return control to shell
+        return;
+    }
+}
+
+void execute_stmt(struct statement s) {
+    if (s.command == NULL) {
+        if (s.operator == 0) {
+            execute_stmt(*s.operands[0]);
+            execute_stmt(*s.operands[1]);
         }
     }
 }
